f1e4ef279a88609e995263c37c94eaf9a24a1647
Greg Taylor <greg.taylor@reddit.com>
2016-05-05 22:39:27 -0700

    Working rough draft GKE Drone 0.5 installer script.

--- gke-with-https/README.md
+++ gke-with-https/README.md
@@ -10,25 +10,43 @@ SSL cert for you at runtime.
 
 There are a few things you'll need to do prior to using these manifests.
 How to do them is beyond the scope of this example, but here's a rough
-run-down (you can do most of this from their web console):
+run-down:
 
-* Create a Container Engine cluster if you don't already have one. You can
-  do this via Google Cloud Platform's web console or the `gcloud` command (if
-  you have installed the GCP SDK).
-* Make sure your ``kubectl`` client is configured correctly to work with
-  your cluster. See the
+### Create a Container Engine Cluster
+
+You'll need to do this if you don't already have one. There are a few different
+ways to do this:
+
+* If you don't have a strong preference, make sure your `gcloud` client is
+  pointed at the project you'd like the cluster created within. Then you
+  can run the `create-gke-cluster.sh` script in this directory. If you do this,
+  you'll end up with a cluster and a persistent disk for your DB.
+* The Google Cloud Platform web console makes cluster creation very easy,
+  as well. See the
   [GKE docs](https://cloud.google.com/container-engine/docs/before-you-begin)),
-  on how to get this set up.
-* Create a persistent disk of 10-20 GB in the same availability zone and
-  region as your cluster. Name it `drone-server-sqlite-db`.
+  on how to get this set up. If you create it like this, you'll need to manually 
+  point your `gcloud` client is pointed at the cluster
 
-### Install Dronoe
+### Create a persistent disk for your sqlite DB
+
+By default, these manifests will store all Drone state on a Google Cloud
+persistent disk via sqlite. As a consequence, we need an empty persistent
+disk before running the installer.
+
+You can either do this in the GCP web console or via the `gcloud` command.
+In the case of the latter, you can use our `create-disk.sh` script after you
+open it up and verify that the options make sense for you.
+
+In either case, make sure the persistent disk is named `drone-server-sqlite-db`.
+Also make sure that it is in the same availability zone as the GKE cluster.
+
+## Install Dronoe
 
 Finally, run `install-drone.sh` and follow the prompts carefully. At the
 end of the installation script, you should be able to point your browser at
 `https://drone.your-fqdn.com` and see a Login page.
 
-### Troubleshooting
+## Troubleshooting
 
 You can verify that everything is running correctly like this:
 
@@ -37,17 +55,31 @@ kubectl get pods
 ```
 
 You should see several pods in a "Running" state. If there were issues,
-note the name of the pod and look at the logs:
+note the name of the pod and look at the logs. This is a close approximation
+of what you'd need to type to check the Caddy proxy logs:
 
 ```
-kubectl logs -f drone-proxy-a123
+kubectl logs -f drone-server-a123 caddy-server
 ```
 
-Where ``drone-proxy-a123`` is the pod name.
+Here's a close approximation of what you'd need to check the Drone server logs:
+
+```
+kubectl logs -f drone-server-a234 drone-server
+```
+
+And agent:
+
+```
+kubectl logs -f drone-agent-a345
+```
+
+Where ``drone-server-a123`` is the pod name.
 
 
-### Stuck? Need help?
+## Stuck? Need help?
 
 We've glossed over quite a few details, for the sake of brevity. If you
 have questions, post them to our [Help!](https://discuss.drone.io/c/help)
-category on the Drone Discussion site.
+category on the Drone Discussion site. If you'd like a more realtime option,
+visit our [Gitter room](https://gitter.im/drone/drone).
--- gke-with-https/install-drone.sh
+++ gke-with-https/install-drone.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
+kubectl cluster-info > /dev/null 2>&1
+if [ $? -eq 1 ]
+then
+  echo "kubectl was unable to reach your Kubernetes cluster. Make sure that" \
+       "you have selected one using the 'gcloud container' commands."
+  exit 1
+fi
 # Clear out the old values, since we're being crude.
 kubectl delete configmap drone 2> /dev/null
 if [ $? -eq 1 ]
 then
-  echo "Before continuing, make sure you have opened drone-configmap.yaml and" \
-       "modified the values to suit your environment."
+  echo "Before continuing, make sure you've opened drone-configmap.yaml and" \
+       "changed the values prefixed with CHANGEME in the comments."
   echo
   read -p "<Press enter once you've made your edits>"
 fi
-kubectl delete rc drone-server 2> /dev/null
-kubectl delete rc drone-proxy 2> /dev/null
 
-kubectl create -f drone-proxy-svc.yaml 2> /dev/null
+kubectl create -f drone-server-svc.yaml 2> /dev/null
 if [ $? -eq 0 ]
 then
   echo "Since this is your first time running this script, we just created a" \
@@ -19,11 +24,11 @@ then
        "for the LB to initialize and pull an IP address. We'll pause for a" \
        "bit and walk you through this after the break."
   while true; do
-    echo "Waiting for 40 seconds for LB creation..."
-    sleep 40
-    echo "[[ Querying your drone-proxy service to see if it has an IP yet... ]]"
+    echo "Waiting for 60 seconds for LB creation..."
+    sleep 60
+    echo "[[ Querying your drone-server service to see if it has an IP yet... ]]"
     echo
-    kubectl describe svc drone-proxy
+    kubectl describe svc drone-server
     echo "[[ Query complete. ]]"
     read -p "Do you see a value above for 'Loadbalancer Ingress'? (y/n) " yn
     case $yn in
@@ -42,10 +47,50 @@ then
   read -p "<Press enter to proceed once nslookup is resolving your proxy FQDN>"
 fi
 
-kubectl create -f drone-server-svc.yaml 2> /dev/null
+kubectl delete rc drone-server 2> /dev/null
 kubectl create -f drone-configmap.yaml
 kubectl create -f drone-server-rc.yaml
-kubectl create -f drone-proxy-rc.yaml
+kubectl create -f drone-server-internal-svc.yaml 2> /dev/null
 echo
-echo "At this point, you should have a running Drone install. Point your" \
-echo "browser at https://<your-fqdn-here> and you should see a login page."
+echo "===== Drone Server installed ============================================"
+echo "You should now have a running Drone Server. Point your browser"
+echo "at https://<your-fqdn-here> and you should see a login page."
+echo
+echo "!!!!! READ ME BEFORE PROCEEDING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "After you have verified that Drone is up and running, you'll need to"
+echo "pull your agent secret from the server logs and edit your configmap."
+echo "First, get a list of your current running pods:"
+echo
+echo "  kubectl get pods "
+echo
+echo "Take the drone-server-* value under NAME and look at your Drone"
+echo "server's logs:"
+echo
+echo "  kubectl logs -f drone-server-<hash> drone-server"
+echo
+echo "Scroll all the way to the top and look for a line that says:"
+echo
+echo "  using agent secret replace-this-with-your-own-random-secret"
+echo
+echo "Copy the token on the line immediately following that one and paste it"
+echo "into your drone-configmap.yaml as the value for agent.secret.token."
+echo "Make sure to save your changes before continuing!"
+echo
+read -p "<Press enter once you've edited and saved agent.secret.token>"
+echo
+echo "Open up drone-agent-rc.yml and find the line starting with 'replicas:'"
+echo "Set this to how many build agents you'd like to run. Each of these can"
+echo "handle one build at a time. Make sure to save your changes!"
+echo
+read -p "<Press enter once you've edited drone-agent-rc.yml>"
+echo
+echo "===== Drone Agent installation =========================================="
+kubectl delete rc drone-agent 2> /dev/null
+kubectl create -f drone-agent-rc.yaml
+echo
+echo "===== Post-installation tasks ==========================================="
+echo "At this point, you should have a fully-functional Drone install. If this"
+echo "Is not the case, stop by either of the following for help:"
+echo
+echo "  * Gitter (realtime chat): https://gitter.im/drone/drone"
+echo "  * Discussion Site, help category: https://discuss.drone.io/c/help"
